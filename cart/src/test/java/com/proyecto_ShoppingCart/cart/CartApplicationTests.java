c433ef8b01930227207ec40a92ff521b4ac9e8aa
adrcur <adrian.curiel@coppel.com>
2025-02-18 12:21:32 -0600

    feat(init): Create base project structure
    This commit sets up the basic folder structure for the project, including files and resources

---
+++ cart/src/test/java/com/proyecto_ShoppingCart/cart/CartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto_ShoppingCart.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
